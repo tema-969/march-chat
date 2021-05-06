5e7c8b1ac3f68b5e8227a5c36e0beff160652ae2
tema-969 <69858987+tema-969@users.noreply.github.com>
2021-05-06 18:27:29 +0300

    HomeWork Level 3 - 2;

--- client/src/main/java/ru/geekbrains/march/chat/client/Controller.java
+++ client/src/main/java/ru/geekbrains/march/chat/client/Controller.java
@@ -1,54 +1,120 @@
 package ru.geekbrains.march.chat.client;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 
-import java.awt.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-
+    @FXML
+    TextField msgField, loginField;
 
     @FXML
-    TextField msgField;
+    PasswordField passwordField;
+
     @FXML
     TextArea msgArea;
+
+    @FXML
+    HBox loginPanel, msgPanel;
+
+    @FXML
+    ListView<String> clientsList;
 
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    private String username;
+
+    public void setUsername(String username) {
+        this.username = username;
+        boolean usernameIsNull = username == null;
+        loginPanel.setVisible(usernameIsNull);
+        loginPanel.setManaged(usernameIsNull);
+        msgPanel.setVisible(!usernameIsNull);
+        msgPanel.setManaged(!usernameIsNull);
+        clientsList.setVisible(!usernameIsNull);
+        clientsList.setManaged(!usernameIsNull);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        setUsername(null);
+    }
+
+    public void login() {
+        if (loginField.getText().isEmpty()) {
+            showErrorAlert("Имя пользователя не может быть пустым");
+            return;
+        }
+
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+
+        try {
+            out.writeUTF("/login " + loginField.getText() + " " + passwordField.getText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void connect() {
         try {
             socket = new Socket("localhost", 8189);
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
             Thread t = new Thread(() -> {
                 try {
+                    // Цикл авторизации
                     while (true) {
                         String msg = in.readUTF();
+                        if (msg.startsWith("/login_ok ")) {
+                            setUsername(msg.split("\\s")[1]);
+                            break;
+                        }
+                        if (msg.startsWith("/login_failed ")) {
+                            String cause = msg.split("\\s", 2)[1];
+                            msgArea.appendText(cause + "\n");
+                        }
+                    }
+                    // Цикл общения
+                    while (true) {
+                        String msg = in.readUTF();
+                        // todo вынести этот блок
+                        if (msg.startsWith("/")) {
+                            if (msg.startsWith("/clients_list ")) {
+                                // /clients_list Bob Max Jack
+                                String[] tokens = msg.split("\\s");
+                                Platform.runLater(() -> {
+                                    clientsList.getItems().clear();
+                                    for (int i = 1; i < tokens.length; i++) {
+                                        clientsList.getItems().add(tokens[i]);
+                                    }
+                                });
+                            }
+                            continue;
+                        }
                         msgArea.appendText(msg + "\n");
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
+                } finally {
+                    disconnect();
                 }
             });
             t.start();
         } catch (IOException e) {
-            throw new RuntimeException("Unable to connect to server [ localhost: 8189 ] ");
+            showErrorAlert("Невозможно подключиться к серверу");
         }
     }
 
@@ -56,9 +122,28 @@ public class Controller implements Initializable {
         try {
             out.writeUTF(msgField.getText());
             msgField.clear();
+            msgField.requestFocus();
         } catch (IOException e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Невозможно отправить сообщение", ButtonType.OK);
-            alert.showAndWait();
+            showErrorAlert("Невозможно отправить сообщение");
         }
+    }
+
+    private void disconnect() {
+        setUsername(null);
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showErrorAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setContentText(message);
+        alert.setTitle("March Chat FX");
+        alert.setHeaderText(null);
+        alert.showAndWait();
     }
 }
---
+++ server/src/main/java/ru/geekbrains/march/chat/server/ClientHandler.java
@@ -0,0 +1,119 @@
+package ru.geekbrains.march.chat.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public ClientHandler(Server server, Socket socket) throws IOException {
+        this.server = server;
+        this.socket = socket;
+        this.in = new DataInputStream(socket.getInputStream());
+        this.out = new DataOutputStream(socket.getOutputStream());
+        new Thread(() -> {
+            try {
+                while (true) { // Цикл авторизации
+                    String msg = in.readUTF();
+                    if (msg.startsWith("/login ")) {
+                        // /login Bob 100xyz
+                        String[] tokens = msg.split("\\s+");
+                        if (tokens.length != 3) {
+                            sendMessage("/login_failed Введите имя пользователя и пароль");
+                            continue;
+                        }
+                        String login = tokens[1];
+                        String password = tokens[2];
+
+                        String userNickname = server.getAuthenticationProvider().getNicknameByLoginAndPassword(login, password);
+                        if (userNickname == null) {
+                            sendMessage("/login_failed Введен некорретный логин/пароль");
+                            continue;
+                        }
+                        if (server.isUserOnline(userNickname)) {
+                            sendMessage("/login_failed Учетная запись уже используется");
+                            continue;
+                        }
+                        username = userNickname;
+                        sendMessage("/login_ok " + username);
+                        server.subscribe(this);
+                        break;
+                    }
+                }
+
+                while (true) { // Цикл общения с клиентом
+                    String msg = in.readUTF();
+                    if (msg.startsWith("/")) {
+                        executeCommand(msg);
+                        continue;
+                    }
+                    server.broadcastMessage(username + ": " + msg);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                disconnect();
+            }
+        }).start();
+    }
+
+    private void executeCommand(String cmd) {
+        // /w Bob Hello, Bob!!!
+        if (cmd.startsWith("/w ")) {
+            String[] tokens = cmd.split("\\s+", 3);
+            if (tokens.length != 3) {
+                sendMessage("Server: Введена некорректная команда");
+                return;
+            }
+            server.sendPrivateMessage(this, tokens[1], tokens[2]);
+            return;
+        }
+
+        // /change_nick myNewNickname
+        if (cmd.startsWith("/change_nick ")) {
+            String[] tokens = cmd.split("\\s+");
+            if (tokens.length != 2) {
+                sendMessage("Server: Введена некорректная команда");
+                return;
+            }
+            String newNickname = tokens[1];
+            if (server.isUserOnline(newNickname)) {
+                sendMessage("Server: Такой никнейм уже занят");
+                return;
+            }
+            server.getAuthenticationProvider().changeNickname(username, newNickname);
+            username = newNickname;
+            sendMessage("Server: Вы изменили никнейм на " + newNickname);
+            server.broadcastClientsList();
+        }
+    }
+
+    public void sendMessage(String message) {
+        try {
+            out.writeUTF(message);
+        } catch (IOException e) {
+            disconnect();
+        }
+    }
+
+    public void disconnect() {
+        server.unsubscribe(this);
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
